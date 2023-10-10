de7ae402c1db88c97bb9518b43f419eba79c13e5
Emanoel Silva <emanoelsilva.jua@gmail.com>
2023-10-10 13:13:55 -0300

    Atualizando

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Aplicação Projeto Web 1</h1>
+      <h1>Busca de Jogadores</h1>
       <SearchBar onSearch={handleSearch} onClear={clearSearch} />
       {showPlayerInfo && <GetPlayerInfo searchQuery={searchQuery} onAdicionarFavorito={adicionarFavorito}/>}
       <hr></hr>
